a169b8a8a6b9fb5329c13bc7ee574e19a204e90f
phatdn <ngocphatnt85@gmail.com>
2024-05-22 23:34:30 +0700

    Create Vans page, VanDetail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
 import './App.css';
+import './server';
 
 import Layout from './components/Layout';
 import About from './pages/About';
 import Home from './pages/Home';
+import Vans from './pages/Vans/Vans';
+import VanDetail from './pages/Vans/VanDetail';
 import NotFound from './pages/NotFound';
 
 const router = createBrowserRouter(
@@ -12,6 +15,8 @@ const router = createBrowserRouter(
 		<Route path="/" element={<Layout />}>
 			<Route index element={<Home />} />
 			<Route path="about" element={<About />} />
+			<Route path="vans" element={<Vans />} />
+			<Route path="vans/:id" element={<VanDetail />} />
 			<Route path="*" element={<NotFound />} />
 		</Route>
 	)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
+	const activeStyles = {
+		fontWeight: 'bold',
+		textDecoration: 'underline',
+		color: '#161616',
+	};
+
 	return (
 		<header>
 			<Link to="/" className="site-logo">
 				#VanLife
 			</Link>
 			<nav>
-				<Link to="about">About</Link>
+				<NavLink to="about" style={({ isActive }) => (isActive ? activeStyles : null)}>
+					About
+				</NavLink>
+				<NavLink to="vans" style={({ isActive }) => (isActive ? activeStyles : null)}>
+					Vans
+				</NavLink>
 			</nav>
 		</header>
 	);
