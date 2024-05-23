57c4efae178290629f46af449cb2854636c5c862
phatdn <ngocphatnt85@gmail.com>
2024-05-23 22:33:50 +0700

    useSearchParams

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 export default function Vans() {
 	const [vans, setVans] = useState([]);
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const typeFilter = searchParams.get('type');
 
 	useEffect(() => {
 		fetch('/api/vans')
@@ -10,7 +13,20 @@ export default function Vans() {
 			.then((data) => setVans(data.vans));
 	}, []);
 
-	const vanElements = vans.map((van) => (
+	const displayedVans = typeFilter ? vans.filter((van) => van.type === typeFilter) : vans;
+
+	function handleFilterChange(key, value) {
+		setSearchParams((prevParams) => {
+			if (value === null) {
+				prevParams.delete(key);
+			} else {
+				prevParams.set(key, value);
+			}
+			return prevParams;
+		});
+	}
+
+	const vanElements = displayedVans.map((van) => (
 		<div key={van.id} className="van-title">
 			<Link to={van.id}>
 				<img src={van.imageUrl} alt="van-img" />
@@ -28,6 +44,31 @@ export default function Vans() {
 	return (
 		<div className="van-list-container">
 			<h1>Explore our van options</h1>
+			<div className="van-list-filter-buttons">
+				<button
+					className={`van-type simple ${typeFilter === 'simple' ? 'selected' : ''}`}
+					onClick={() => handleFilterChange('type', 'simple')}
+				>
+					Simple
+				</button>
+				<button
+					className={`van-type luxury ${typeFilter === 'luxury' ? 'selected' : ''}`}
+					onClick={() => handleFilterChange('type', 'luxury')}
+				>
+					Luxury
+				</button>
+				<button
+					className={`van-type rugged ${typeFilter === 'rugged' ? 'selected' : ''}`}
+					onClick={() => handleFilterChange('type', 'rugged')}
+				>
+					Rugged
+				</button>
+				{typeFilter ? (
+					<button className="van-type clear-filters" onClick={() => handleFilterChange('type', null)}>
+						Clear filters
+					</button>
+				) : null}
+			</div>
 			<div className="van-list">{vanElements}</div>
 		</div>
 	);
